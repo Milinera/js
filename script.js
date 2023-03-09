a8ee352b2a8fb13ea77491250bde8acb2a2a1c23
Milinera <MilineraGrov@gmail.com>
2023-03-09 20:06:58 +0300

    less35.1

--- script.js
+++ script.js
@@ -302,3 +302,82 @@
 //     return x
 // }
 // console.log(calcFibanachi(14, 2, 3))
+
+// const personalPlanPeter = {
+//     name: "Peter",
+//     age: "29",
+//     skills: {
+//         languages: ['ru', 'eng'],
+//         programmingLangs: {
+//             js: '20%',
+//             php: '10%'
+//         },
+//         exp: '1 month'
+//     },
+//     showAgeAndLangs: function (plan) {
+//         let {name, age, skills:{languages, programmingLangs, programmingLangs:{js, php}, exp}} = plan
+//         console.log (`Мне ${age} и я владею языками: ${languages}`)
+//     }
+// };
+
+// function showExperience(plan) {
+//     let {name, age, skills:{languages, programmingLangs, programmingLangs:{js, php}, exp}} = plan
+//     console.log (programmingLangs)
+// }
+
+// function showProgrammingLangs(plan) {
+//     let {name, age, skills:{languages, programmingLangs, programmingLangs:{js, php}, exp}} = plan
+//     a = (`Язык js изучен на ${programmingLangs.js}`)
+//     console.log(a)
+// }
+
+// showExperience(personalPlanPeter);
+// showProgrammingLangs(personalPlanPeter)
+// personalPlanPeter.showAgeAndLangs(personalPlanPeter)
+
+// dsaaaaaaaaaaaaaaaaaaaaaaa
+
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(plan) {
+        const {age} = plan;
+        const {languages} = plan.skills;
+        let str = `Мне ${age} и я владею языками: `;
+
+        languages.forEach(function(lang) {
+            str += `${lang.toUpperCase()} `;
+        });
+
+        return str;
+    }
+};
+
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
+
+function showExperience(plan) {
+    const {exp} = plan.skills;
+    return exp;
+}
+
+showExperience(personalPlanPeter);
+
+function showProgrammingLangs(plan) {
+    let str = '';
+    const {programmingLangs} = plan.skills;
+    for (let key in programmingLangs) {
+        str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
+    }
+
+    return str;
+}
+
+showProgrammingLangs(personalPlanPeter);
